701e5556a303ace81040acf23ef00ce8a44e5a10
jasurn <97jasur@gmail.com>
2020-02-21 15:01:04 +0500

    resolved wrong naming and updated gitignore

--- dwproone/asgi.py
+++ dwproone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dwproone_backend project.
+ASGI config for dwproone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dwproone_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dwproone.settings')
 
 application = get_asgi_application()
--- dwproone/urls.py
+++ dwproone/urls.py
@@ -1,4 +1,4 @@
-"""dwproone_backend URL Configuration
+"""dwproone URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
