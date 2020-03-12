e82ee4a762e709403d864744aebfa44bdab5411c
jasurn <97jasur@gmail.com>
2020-03-12 14:53:23 +0500

    1. updated bug with bash_profiles name

--- productionServerRun.sh
+++ productionServerRun.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source ~/dwproone_backend/env_dwproone/bin/activate
-source ~/.bash_profile
-
+source ~/.bash_profiles
 python ~/dwproone_backend/dwproone/manage.py runserver 0.0.0.0:8000
