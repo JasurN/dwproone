f1e5f4875e264c2ac2b63bd61043e5da38b788b7
jasurn <97jasur@gmail.com>
2020-03-12 17:10:58 +0500

    1. added documentation about server setup 2. changed prod allowed host to local only

--- dwproone/settings/prod.py
+++ dwproone/settings/prod.py
@@ -1,7 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['localhost', '46.101.192.185']
-DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
