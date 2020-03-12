bc981ce8dab37ecbbf62c69bc32a2ca22aaebce6
jasurn <97jasur@gmail.com>
2020-03-12 14:50:23 +0500

    1. testing hosts 2. added productionServerRun.sh

--- developmentServerRun.sh
+++ developmentServerRun.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo systemctl stop nginx
+source ~/.bash_profile
 python manage.py runserver
---
+++ productionServerRun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ~/dwproone_backend/env_dwproone/bin/activate
+source ~/.bash_profile
+python manage.py runserver 0.0.0.0:8000
