394995b8c2ab526be2c4e178b1efff8765a4e37c
jasurn <97jasur@gmail.com>
2020-06-05 11:22:53 +0500

    frontend: -initial react-admin integration with backend

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,24 +1,23 @@
 // in src/App.js
 import React from 'react';
 
-import {Admin, Resource} from 'react-admin';
-import {UserList} from "./users";
-import jsonServerProvider from 'ra-data-json-server';
-import {PostList} from "./PostList";
-import {PostEdit} from "./PostEdit";
-import {PostCreate} from "./PostCreate";
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
-import Dashboard from "./Dashboard";
-import authProvider from './authProvider';
+import {Admin, Resource, ListGuesser} from 'react-admin';
+// import {UserList} from "./users";
+// import jsonServerProvider from 'ra-data-json-server';
+// import {PostList} from "./PostList";
+// import {PostEdit} from "./PostEdit";
+// import {PostCreate} from "./PostCreate";
+// import PostIcon from '@material-ui/icons/Book';
+// import UserIcon from '@material-ui/icons/Group';
+// import Dashboard from "./Dashboard";
+// import authProvider from './authProvider';
+import drfProvider from './dataprovider/index'
 
-
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = drfProvider('https://localhost/api');
 const App = () => (
-    <Admin dashboard={Dashboard} authProvider={authProvider}  dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
-        <Resource name="users" list={UserList} icon={UserIcon}/>
-    </Admin>
+   <Admin dataProvider={dataProvider}>
+      <Resource name="warehouse/rolls" list={ListGuesser} />
+   </Admin>
 );
 
 export default App;
