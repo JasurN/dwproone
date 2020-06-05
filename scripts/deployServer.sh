bb31ba6a0bdc0390fb6cf2bfd6e6f037fed373c9
jasurn <97jasur@gmail.com>
2020-06-05 11:51:47 +0500

    General: -updated deployServer.sh added pip install -r requirements.txt

--- scripts/deployServer.sh
+++ scripts/deployServer.sh
@@ -4,6 +4,8 @@ npm install
 npm run build
 source ~/env_dw/bin/activate
 source ~/.bash_profile
+cd ~/dwproone
+pip install -r requirements.txt
 python ~/dwproone/manage.py makemigrations
 python ~/dwproone/manage.py migrate
 python ~/dwproone/manage.py collectstatic --noinput
