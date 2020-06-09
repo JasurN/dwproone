944d51fb8842eba8f4e49a6ccbd183f826d30227
jasurn <97jasur@gmail.com>
2020-06-09 13:53:01 +0500

    frontend: -add filter for format

--- frontend2/src/Components/Warehouse/RollsList.jsx
+++ frontend2/src/Components/Warehouse/RollsList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {List, Datagrid, TextField, NumberField, DateField, Filter, TextInput, ReferenceInput, SelectInput} from 'react-admin';
+import {List, Datagrid, TextField, NumberField, DateField, EditButton, Filter, TextInput, ReferenceInput, SelectInput} from 'react-admin';
 
 const RollsFilter = (props) => (
     <Filter {...props}>
@@ -7,13 +7,13 @@ const RollsFilter = (props) => (
         <ReferenceInput label="Format" source="paper__paper_format_id" reference="warehouse/papers/formats" allowEmpty>
             <SelectInput optionText="format" />
         </ReferenceInput>
-
     </Filter>
 );
 
 export const RollsList = props => (
     <List  filters={<RollsFilter/>} {...props}>
         <Datagrid>
+            {/*<TextField source="id"/>*/}
             <TextField source="roll_id"/>
             <NumberField source="initial_weight"/>
             <NumberField source="current_weight"/>
@@ -22,7 +22,7 @@ export const RollsList = props => (
             <TextField source="paper.grammage.grammage" label="Grammage"/>
             <TextField source="paper.paper_format.format" label="Format"/>
             <TextField source="paper.company.name" label="Company"/>
-
+            <EditButton />
         </Datagrid>
     </List>
 );
