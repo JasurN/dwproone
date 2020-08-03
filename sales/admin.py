f2b591ba1f5fadf91ad38edf8bc558cddaa83273
jasurn <97jasur@gmail.com>
2020-08-03 18:49:50 +0500

    -initial migrations for sales -add __str__ to models for admin site

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Box, Contract, Customer, Order
+
+admin.site.register(Box)
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(Contract)
 # Register your models here.
