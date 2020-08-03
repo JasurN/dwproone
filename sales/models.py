30170c2224bf8054c1b073db32e35a9d1efec95d
jasurn <97jasur@gmail.com>
2020-08-03 17:37:34 +0500

    -initial model for sales -

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Box(models.Model):
+    length = models.PositiveSmallIntegerField(help_text='box length')
+    width = models.PositiveSmallIntegerField(help_text='box width')
+    height = models.PositiveSmallIntegerField(help_text='box height')
+    configuration = models.CharField(max_length=10, help_text='box configuration, KSS SSS KSSSK')
+
+
+class Customer(models.Model):
+    name = models.CharField(help_text='customer name')
+
+
+class Contract(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    contract_number = models.CharField(max_length=200, help_text='contract number DPC LB 190503')
+
+
+# class Order(models.Model):
+#     contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+#
--- sales/urls.py
+++ sales/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'sales'
+# api/sales/
 urlpatterns = [
     path('', views.index),
 ]
