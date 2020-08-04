05f7ded73160c86887ec7ebc0a43acc127c3b979
jasurn <97jasur@gmail.com>
2020-08-04 15:17:26 +0500

    -add serializer, view for boxes, customers

--- sales/urls.py
+++ sales/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import AllOrdersListView
+from .views import AllOrdersListView, AllBoxListView, AllCustomerListView
 
 app_name = 'sales'
 # api/sales/
 urlpatterns = [
     path('orders/all/', AllOrdersListView.as_view()),
+
+]
+
+urlpatterns += [
+    path('box/all/', AllBoxListView.as_view()),
+]
+
+urlpatterns += [
+    path('customer/all/', AllCustomerListView.as_view()),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
 
-from sales.models import Order
-from sales.serializers import AllOrdersSerializer
+from sales.models import Order, Box, Customer
+from sales.serializers import AllOrdersSerializer, AllBoxSerializer, AllCustomerSerializer
 
 
 class AllOrdersListView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = AllOrdersSerializer
+
+
+class AllBoxListView(generics.ListCreateAPIView):
+    queryset = Box.objects.all()
+    serializer_class = AllBoxSerializer
+
+
+class AllCustomerListView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = AllCustomerSerializer
