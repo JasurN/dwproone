45179f7496af32f16bf8d7475f1580780a5897fd
jasurn <97jasur@gmail.com>
2020-08-15 00:16:30 +0500

    Order list view test -add test for check count from response -add order utility

--- sales/tests/test_view.py
+++ sales/tests/test_view.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, force_authenticate
-from sales.tests.sales_test_utilites import create_customers, create_user, create_boxes, create_contract
-from sales.views import CustomerListView, BoxListCreateView, ContractListCreateView
+
+from sales.models import Order, Customer, Box, Contract
+from sales.tests.sales_test_utilites import create_customers, create_user, create_boxes, create_contract, create_order
+from sales.views import CustomerListView, BoxListCreateView, ContractListCreateView, OrdersListView
 
 
 class CustomerListViewTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        create_customers(number_customers=10, name='Customer')
+        create_customers(count=10)
         create_user()
 
     def test_customer_list_view_authentication(self):
@@ -23,6 +25,15 @@ class CustomerListViewTestCase(TestCase):
         force_authenticate(request, user=user)
         response = view(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_customer_list_count_is_10(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='testuser1')
+        request = factory.get('/api/sales/customer/all/')
+        view = CustomerListView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.data['count'], 10)
 
 
 class BoxListViewTestCase(TestCase):
@@ -45,6 +56,15 @@ class BoxListViewTestCase(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, 200)
 
+    def test_box_list_count_is_10(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='testuser1')
+        request = factory.get('/api/sales/box/all/')
+        view = BoxListCreateView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.data['count'], 10)
+
 
 class ContractListViewTestCase(TestCase):
     @classmethod
@@ -53,7 +73,7 @@ class ContractListViewTestCase(TestCase):
         create_contract(count)
         create_user()
 
-    def test_box_list_view_authentication(self):
+    def test_contract_list_view_authentication(self):
         response = self.client.get('/api/sales/contract/all/')
         self.assertEqual(response.status_code, 401)
 
@@ -65,3 +85,42 @@ class ContractListViewTestCase(TestCase):
         force_authenticate(request, user=user)
         response = view(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_contract_list_count_is_10(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='testuser1')
+        request = factory.get('/api/sales/contract/all/')
+        view = ContractListCreateView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.data['count'], 10)
+
+
+class OrderListViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        count = 10
+        create_order(count)
+        create_user()
+
+    def test_order_list_view_authentication(self):
+        response = self.client.get('/api/sales/orders/all/')
+        self.assertEqual(response.status_code, 401)
+
+    def test_order_list_view_exists_at_desired_local(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='testuser1')
+        request = factory.get('/api/sales/orders/all/')
+        view = OrdersListView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_order_list_count_is_10(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='testuser1')
+        request = factory.get('/api/sales/orders/all/')
+        view = OrdersListView.as_view()
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.data['count'], 10)
