b531a1f5a6d638f37b2ea110bb7034d270c6a7a3
jasurn <97jasur@gmail.com>
2020-08-24 16:41:12 +0500

    Add production schedule

--- production/serializers.py
+++ production/serializers.py
@@ -8,3 +8,11 @@ class ProductionSerializer(serializers.ModelSerializer):
         model = Production
         fields = '__all__'
         depth = 4
+
+
+class AddProductionSerializer(serializers.ModelSerializer):
+    order_id = serializers.IntegerField(source='order.id')
+
+    class Meta:
+        model = Production
+        fields = ['order_id', 'scheduled_date', 'production_order']
--- production/views.py
+++ production/views.py
@@ -1,12 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from production.models import Production
-from production.serializers import ProductionSerializer
+from production.serializers import ProductionSerializer, AddProductionSerializer
 
 
 class ProductionListCreateView(generics.ListCreateAPIView):
     queryset = Production.objects.all()
     serializer_class = ProductionSerializer
+
+    def create(self, request, *args, **kwargs):
+        production_serializer = AddProductionSerializer(data=request.data)
+        production_serializer.is_valid(raise_exception=True)
+        production = Production.objects.create(order_id=production_serializer.data.get('order_id'),
+                                               scheduled_date=production_serializer.data.get('scheduled_date'),
+                                               production_order=production_serializer.data.get('production_order'))
+        return Response(data=ProductionSerializer(production).data, status=status.HTTP_201_CREATED)
 
 
 class CorrugatorHistoryListCreateView(generics.ListCreateAPIView):
