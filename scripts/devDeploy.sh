223635c3d3c255c92433f2b2fa07632067111c5c
jasurn <97jasur@gmail.com>
2020-10-01 00:36:43 +0500

    update devDeploy.sh with npm due to ram memory overflow

--- scripts/devDeploy.sh
+++ scripts/devDeploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 pipenv run python manage.py test &&
 cd ../dw_frontend &&
-  yarn install &&
-  yarn build &&
+  npm install &&
+  npm run build &&
   mkdir -p ~/PycharmProjects/dwproone/frontend/production &&
   cp -r build/* ~/PycharmProjects/dwproone/frontend/production/ &&
   cd ~/PycharmProjects/dwproone &&
-  pipenv run python manage.py makemigrations &&
   pipenv run python manage.py migrate &&
   pipenv run python manage.py collectstatic --noinput
