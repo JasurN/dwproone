e4833b1c34eaa97d249b7f919340949c041cbf98
jasurn <97jasur@gmail.com>
2020-10-02 19:53:21 +0500

    change deployServer.sh script -delete db dump

--- scripts/deployServer.sh
+++ scripts/deployServer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-  sudo touch emptyFile
-  git pull &&
+sudo touch emptyFile
+git pull &&
   cd ../dw_frontend &&
-    git pull &&
-    mkdir -p ~/dwproone/frontend/production/ &&
-    cp -r build/* ~/dwproone/frontend/production/ &&
-    cd ~/dwproone &&
-    pipenv install &&
-    pipenv run python ~/dwproone/manage.py migrate &&
-    pipenv run python ~/dwproone/manage.py test &&
-    pipenv run python ~/dwproone/manage.py collectstatic --noinput &&
-    sudo systemctl restart erp
+  git pull &&
+  yarn install &&
+  yarn build &&
+  mkdir -p ~/dwproone/frontend/production/ &&
+  cp -r build/* ~/dwproone/frontend/production/ &&
+  cd ~/dwproone &&
+  pipenv install &&
+  pipenv run python ~/dwproone/manage.py migrate &&
+  pipenv run python ~/dwproone/manage.py test &&
+  pipenv run python ~/dwproone/manage.py collectstatic --noinput &&
+  sudo systemctl restart erp
--- scripts/devDeploy.sh
+++ scripts/devDeploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 pipenv run python manage.py test &&
-cd ../dw_frontend &&
-  npm install &&
-  npm run build &&
+  cd ../dw_frontend &&
+  yarn install &&
+  yarn build &&
   mkdir -p ~/PycharmProjects/dwproone/frontend/production &&
   cp -r build/* ~/PycharmProjects/dwproone/frontend/production/ &&
-  rm -r build/*
-  cd ~/PycharmProjects/dwproone &&
+  rm -r build/
+cd ~/PycharmProjects/dwproone &&
   pipenv run python manage.py migrate &&
   pipenv run python manage.py collectstatic --noinput
